9d6036dd113a46447cd80f6a0cd62ab8f446e2cd
JuliusFaj <funnsojulius16@gmail.com>
2023-04-19 13:11:46 -0700

    changing to another project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
 import { useGlobalContext } from "./context";
-import Result from "./result";
 
 function App() {
-  const { search, setSearch } = useGlobalContext();
-
-  return (
-    <section className="container">
-      <div className="title">
-        <p>lyric-finder</p>
-      </div>
-      <div className="form-container">
-        <form
-          className="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            setSearch("");
-          }}
-        >
-          <input
-            type="search"
-            name="search"
-            id=""
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="search lyric"
-          />
-        </form>
-      </div>
-
-      <div className="result-container">
-        <Result />
-      </div>
-    </section>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
--- src/context.js
+++ src/context.js
@@ -1,52 +1,21 @@
 import React from "react";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useReducer } from "react";
+
+import reducer from "./reducer";
 
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+  total: 0,
+  totalAmount: 0,
+};
+
 const AppProvider = ({ children }) => {
-  //for getting the track
-  const [searchData, setSearchData] = useState([]);
-  const [search, setSearch] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "c830865482mshad14dc47e9ab6cdp129804jsn836f7ea3e592",
-      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-    },
-  };
-
-  const getData = () => {
-    setIsLoading(true);
-    fetch(
-      `https://spotify23.p.rapidapi.com/search/?q=${search}type=tracks&offset=0&limit=10&numberOfTopResults=5`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        const { tracks } = response;
-        const { items } = tracks;
-
-        console.log(items);
-        setSearchData(items);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setSearchData([]);
-        setIsLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, [search]);
-
-  return (
-    <AppContext.Provider value={{ search, setSearch, isLoading, searchData }}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
